959f88237ff4566c61166a71db75c5ec4b894d7f
Devin Cooley and Nikhil Batta <nikhil@>
2019-10-01 08:28:38 -0700

    started Program page and method page

---
+++ todo/Models/Script.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Models
+{
+    class Script
+    {
+        
+    }
+}
---
+++ todo/program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+
+namespace ToDoList 
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write
+        }
+    }
+}
